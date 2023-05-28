acaf22a35eeb9bda1c9beca7cacf45c5704640f1
Sherman <shermantaymk@gmail.com>
2023-05-28 10:35:26 +0800

    create value and variables js lesson

---
+++ Javascript Fundamentals/valueAndVariables.js
@@ -0,0 +1,38 @@
+// Values and Variables
+
+// Values
+console.log('Jonas');
+console.log(23);
+
+// Variables
+let firstName = "Bob";
+// Camel case - firstNamePerson 
+console.log(firstName);
+
+// variable name can only contain numbers,letters,underscore and dollar sign
+
+// for constants, caps all 
+let PI = 3.1415;
+
+// variables names should be easily understood
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// Bad example
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+/*
+Assignment 1  
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country(population in millions)
+2. Log their values to the console 
+*/
+
+let country = "Singapore";
+let continent = "Asia";
+let population = 5.454;
+
+console.log(country)
+console.log(continent)
+console.log(population)
