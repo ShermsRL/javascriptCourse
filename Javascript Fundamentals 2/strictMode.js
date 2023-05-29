c58d986f82c67499c32102f002c371c9a05bfc15
Sherman <shermantaymk@gmail.com>
2023-05-29 12:35:07 +0800

    add strict mode js lesson

---
+++ Javascript Fundamentals 2/strictMode.js
@@ -0,0 +1,13 @@
+'use strict';
+// forbids us to do certain things
+// will show a error on console log where without strict mode will not show
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// some reserved word for the future thatn cant be used
+// const interface = 'Audio';
+// const private = 534;
