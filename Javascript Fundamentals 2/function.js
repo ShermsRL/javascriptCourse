3b5e5c1e8410d7877096ae643d349e14a6a034f8
Sherman <shermantaymk@gmail.com>
2023-05-29 12:48:23 +0800

    add function js lesson

---
+++ Javascript Fundamentals 2/function.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function logger() {
+    console.log('My name is Sherman');
+}
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+// passing data and returning data from function
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
