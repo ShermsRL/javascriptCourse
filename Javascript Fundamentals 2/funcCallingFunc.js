03fd359d9e2e1fd9694d8d8cc8f213ee70d0ab59
Sherman <shermantaymk@gmail.com>
2023-05-29 13:28:36 +0800

    add function calling other function js lesson

---
+++ Javascript Fundamentals 2/funcCallingFunc.js
@@ -0,0 +1,15 @@
+// Function calling other function
+
+function fruitCutter(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = fruitCutter(apples);
+    const orangePieces = fruitCutter(oranges);
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
