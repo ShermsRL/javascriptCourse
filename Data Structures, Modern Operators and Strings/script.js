9294020c984dcdfdfe79fddb84bf448f88595b73
Sherman <shermantaymk@gmail.com>
2023-08-03 15:25:57 +0800

    add ES6 enhanced object literals lesson

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -4,6 +4,23 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -11,45 +28,42 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // ES6 enhanced object literals
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
   // destructuring from obj that is passed in
-  orderDelievery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelievery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delievered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
-
+  // dont have to use function keyword for enhanced object literals
   orderPizza: function (mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  // openingHours: {
+  //   thu: {
+  //     open: 12,
+  //     close: 22,
+  //   },
+  //   fri: {
+  //     open: 11,
+  //     close: 23,
+  //   },
+  //   sat: {
+  //     open: 0, // Open 24 hours
+  //     close: 24,
+  //   },
+  // },
 };
 
 // Destructuring Arrays - use []
@@ -94,8 +108,8 @@ console.log(p, q, r);
 // ----------------------------------------------------------------------
 
 // Destructuring Objects - use {}
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+const { name, openingHours2, categories } = restaurant;
+console.log(name, openingHours2, categories);
 
 // changing the variable name for the selected elements
 const {
@@ -165,7 +179,20 @@ console.log(letters);
 // const ingredients = [
 //   prompt("Let's make pasta! Ingredient 1?"),
 //   prompt('Ingredient 2?'),
-//   prompt('Ingredient 3?'),
+//   prompt('Ingredient 3?'),openingHours: {
+//   thu: {
+//     open: 12,
+//     close: 22,
+//   },
+//   fri: {
+//     open: 11,
+//     close: 23,
+//   },
+//   sat: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// },
 // ];
 // console.log(ingredients);
 
@@ -196,8 +223,8 @@ const [pizza, , risotto, ...otherFood] = [
 console.log(pizza, risotto, otherFood);
 
 // objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(sat, weekdays);
+const { sat, ...weekdays2 } = restaurant.openingHours;
+console.log(sat, weekdays2);
 
 // rest operator on functions
 const add = function (...numbers) {
@@ -301,3 +328,7 @@ for (const item of menu3) console.log(item);
 for (const [i, el] of menu3.entries()) {
   console.log(`${i + 1}: ${el}`);
 }
+
+// ----------------------------------------------------------------------
+
+// Enhanced Object literals
