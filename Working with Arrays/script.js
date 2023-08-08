c54a3d6352102a50c6782dda852cee27a130b2d4
Sherman <shermantaymk@gmail.com>
2023-08-08 15:26:35 +0800

    add foreach method lesson js

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -64,15 +64,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 // Array methods
@@ -119,3 +110,42 @@ console.log(arr3.slice(-1)[0]);
 console.log(arr.at(-1));
 // works for str as well
 console.log('sherman'.at(-1));
+
+// Looping arr with forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---FOR EACH ---');
+// first parameter is the element and second is the index compared to for of loop
+// cannot break out of for each loop even with break
+movements.forEach(function (movement, i, arr) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// forEach with maps and sets
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+currenciesUnique.forEach(function (value) {
+  // key === value because set have no key
+  console.log(`${value}`);
+});
