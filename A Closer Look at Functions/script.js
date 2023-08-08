3b1aa6a9d3a134f8de55638db0a8a06bacf729e5
Sherman <shermantaymk@gmail.com>
2023-08-08 09:08:04 +0800

    add default parameters lesson js

--- A Closer Look at Functions/script.js
+++ A Closer Look at Functions/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Default Parameters
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // old way (ES5) way for default parameters
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+// if you want to leave one of the arguments blank
+createBooking('LH123', undefined, 1000);
