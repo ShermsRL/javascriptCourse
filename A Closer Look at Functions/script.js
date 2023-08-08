c74c78c4beeb87a722537970b658b7b4051941c5
Sherman <shermantaymk@gmail.com>
2023-08-08 14:47:26 +0800

    add closures lesson js

--- A Closer Look at Functions/script.js
+++ A Closer Look at Functions/script.js
@@ -206,3 +206,64 @@ runOnce();
 })();
 
 (() => console.log('This will never run again'))();
+
+// Closures - not a feature we explicitly use, JS does it automatically, cannot access the variables, but can look at it
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+// stored in scopes > closure
+console.dir(booker);
+
+// More closure examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// reassigning f, old closure disappear
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// closure have priority over scope chain
+const perGroup = 1000;
+boardPassengers(180, 3);
