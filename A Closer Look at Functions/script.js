54c92e9f579962cf9f32f79fa606ecb0e9c6a1a4
Sherman <shermantaymk@gmail.com>
2023-08-08 09:59:58 +0800

    add apply and call method lesson js

--- A Closer Look at Functions/script.js
+++ A Closer Look at Functions/script.js
@@ -102,3 +102,52 @@ greet('Hello')('Sherman');
 // Challenge
 const greet2 = greeting => name => console.log(`${greeting} ${name}`);
 greet2('Hello')('Sherms');
+
+// The call and apply methods
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Sherman Tay');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+// Does not work
+// book(23, 'Sarah Williams');
+// the call method tells the this keyword which object to point to
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 582, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method - same as call method but takes an array
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
