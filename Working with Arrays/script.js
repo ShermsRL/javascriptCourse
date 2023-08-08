cebeec5766c9b6b504b7821912e11ea211a66fae
Sherman <shermantaymk@gmail.com>
2023-08-08 15:02:22 +0800

    add array methods lesson js

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -74,3 +74,36 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// Array methods
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice - does not mutate the og arr
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+// creating a shallow copy
+console.log(arr.slice());
+console.log([...arr]);
+
+// Splice -mutate the og arr
+// console.log(arr.splice(2));
+arr.splice(-1);
+arr.splice(1, 2); // starting element, no. of element to delete
+console.log(arr);
+
+// Reverse - mutate the og arr
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat - does not mutate the og arr
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// Join
+console.log(letters.join('-'));
