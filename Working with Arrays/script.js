b6ef315e26ae6604c916f0cc785a7e5a6cf96d62
Sherman <shermantaymk@gmail.com>
2023-08-09 15:10:30 +0800

    add loan function to bankist app with some method

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -172,6 +172,19 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   if (
@@ -366,3 +379,21 @@ console.log(firstWithdrawal);
 console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
+
+// some and every method
+// includes check equality
+console.log(movements.includes(-130));
+
+// some checks condition
+const anyDeposits = movements.some(mov => mov > 5000);
+console.log(anyDeposits);
+
+// every - return true if all elements is true
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
