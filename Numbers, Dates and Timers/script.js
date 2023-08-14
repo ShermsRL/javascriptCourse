ea1c4f89ab03d0f6ab6daf8a73b1a9e4ee0f45b7
Sherman <shermantaymk@gmail.com>
2023-08-14 17:27:25 +0800

    add math and rounding lesson js

--- Numbers, Dates and Timers/script.js
+++ Numbers, Dates and Timers/script.js
@@ -284,3 +284,40 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
+
+// Math and Rounding
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+// max does type conversion
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3));
+console.log(Math.round(23.9));
+
+// round up
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+//round down
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+// Rounding decimals
+// toFixed will return string, toFixed to specify how many decimal points
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+// converting to number
+console.log(+(2.345).toFixed(2));
