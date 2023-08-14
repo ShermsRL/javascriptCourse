fd7d474555dd55a2c31df56932c6a330c5775bab
Sherman <shermantaymk@gmail.com>
2023-08-14 20:07:25 +0800

    add numeric separators lesson js

--- Numbers, Dates and Timers/script.js
+++ Numbers, Dates and Timers/script.js
@@ -338,3 +338,23 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+
+// Numeric separators
+// 287,460,000,000
+// use underscore to make numbers more readable
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transfer = 15_00;
+
+// underscore must be between numbers, no symbols before or after
+const PI = 3.14_15;
+console.log(PI);
+
+// underscore in str will not be read as numbers
+// console.log(Number('230_000'));
+
+//
